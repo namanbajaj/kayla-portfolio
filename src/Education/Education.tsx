5af0442e0e543f22f64168ef3afffba3711dfa87
namanbajaj <namanbajaj05@gmail.com>
2024-06-28 00:39:36 -0400

    education

--- src/Education/Education.tsx
+++ src/Education/Education.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
 import './Education.css';
 
-function Education()  {
+const data = [
+  'Google Ads/Analytics',
+  'Canva & Capcut',
+  'Trello & Buffer',
+  'Qualtrics',
+  'HTML & JavaScript',
+  'Outbrain & Taboola',
+  'Microsoft Ads',
+  'Meta Business Suite',
+  'Cision',
+  'Jun Group',
+  'Constant Contact, Hootsuite, & MailChimp',
+  'Microsoft/Google Suites',
+  'Adobe Suite',
+  'Social Media: (Instagram, Facebook, Tiktok, Reddit, Twitter, Pinterest)'
+]
+
+function Education() {
   return (
-    <section id='education'>Education</section>
+    <section id='education'>
+      <div className='education_container'>
+        <h1 className='edu_title'>Education and Skills</h1>
+        <div className='education_grid'>
+          <div className='education_list'>
+            <p>Rutgers University</p>
+            <ul>
+              <li><b>Major: </b>Human Resources Management  & Communication w/ a specialization in Strategic Public Relations</li>
+              <li><b>Graduation Date: </b>May 2024</li>
+              <li><b>GPA: </b>3.4/4.0</li>
+            </ul>
+          </div>
+          <div className='skills_list'>
+            {data.map((item, index) => (
+              <div key={index} className='skill_item'>
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+
+    </section>
   );
 };
 
